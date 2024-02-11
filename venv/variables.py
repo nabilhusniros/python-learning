66659927d2d648eb8ff837be887072ee2cb03d23
nabilhusniros <nabilhusnir@gmail.com>
2024-02-11 12:21:10 +0800

    Initial commit

---
+++ venv/variables.py
@@ -0,0 +1,27 @@
+# Learning about variables
+
+_cars = 23
+cars = 23
+CARS = 23
+number_of_cars = 23
+type_of_cars = 'Myvi'
+
+print(cars)
+print(_cars)
+print(CARS)
+print(number_of_cars)
+print(type_of_cars)
+
+# Learning about strings
+
+"""""
+This is a multi line comment. Use this to add a comment of your code
+"""""
+
+name = 'Nabil Husni'
+type_of_cars = 'Axia'
+location = 'Kuala Lumpur'
+
+#Python formatting method
+
+print('{} is at {} driving an {}.'.format(name, location, type_of_cars))
