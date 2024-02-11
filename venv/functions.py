cc1dc1df4be93a96e87468876decf32af879d0c3
nabilhusniros <nabilhusnir@gmail.com>
2024-02-11 22:03:26 +0800

    Adding script function

---
+++ venv/functions.py
@@ -0,0 +1,15 @@
+#How to write a function
+def addition():
+    a = 1.23
+    b = 10.77
+    print(a + b)
+
+addition()
+
+#Input function
+def plus():
+    a = int(input("Enter a number. "))
+    b = int(input("Enter another number. "))
+    print(a + b)
+
+plus()
